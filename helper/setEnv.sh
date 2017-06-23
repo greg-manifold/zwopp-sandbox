5ac3962d0a084397f28d2cafaf595acd2c332b9a
greg-manifold <greg@manifoldtechnology.com>
2017-06-23 16:17:11 -0700

    Build changes

---
+++ helper/setEnv.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Not the right endpoint. How do we get the path to the newly generated endpoint for this build?
+export TODOS_ENDPOINT=https://ecvzwmgylk.execute-api.us-east-2.amazonaws.com/beta
+
+echo $TODOS_ENDPOINT

c047b2a1c209f3a5e5e6d017904b2786e6e93094
greg-manifold <greg@manifoldtechnology.com>
2017-06-27 15:56:21 -0700

    Adjusted start index on string parse

--- helper/getEndpoint.js
+++ helper/getEndpoint.js
@@ -10,9 +10,9 @@ fs.readFile(__dirname + '/../.build/deploy.out', 'utf8', function (err,data) {
   var lines = data.split("\n");
   for (var i = 0; lines.length > i; i++) {
     if(lines[i].indexOf("POST") > -1) {
-	  var startIndex = lines[i].indexOf("https://") + 8;
+	  //var startIndex = lines[i].indexOf("https://") + 8;
 	  var endIndex = lines[i].indexOf("/todos");
-	  console.log(lines[i].substring(startIndex, endIndex));
+	  console.log(lines[i].substring(0, endIndex));
 	  i = lines.length;
     }
   }

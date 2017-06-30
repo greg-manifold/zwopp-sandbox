11d863162c4f11078a89c8cb30dce82bcfe1a24a
greg-manifold <greg@manifoldtechnology.com>
2017-06-30 16:14:40 -0700

    Modified base url 2

--- helper/getEndpoint.js
+++ helper/getEndpoint.js
@@ -9,7 +9,7 @@ fs.readFile(__dirname + '/../.build/deploy.out', 'utf8', function (err,data) {
 
   var lines = data.split("\n");
   for (var i = 0; lines.length > i; i++) {
-    if(lines[i].indexOf("POST") > -1) {
+    if(lines[i].includes("POST") && lines[i].includes("execute-api")) {
 	  var startIndex = lines[i].indexOf("https://");
     var slash1 = lines[i].indexOf("/", startIndex + 8);
     var slash2 = lines[i].indexOf("/", slash1 + 1);
